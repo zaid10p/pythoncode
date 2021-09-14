88fd0f39300e487e0ce1451839ddfc4f7a75b6b2
Zaid Ibrahim <zaid.ibrahim@10pearls.com>
2021-09-14 17:56:05 +0500

    decorator code example

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from data_structure.stack import Stack
+from functools import wraps
 
 
-st = Stack([2,3])
+st = Stack([2, 3])
 st.pop()
 st.push(5)
 print(st.getList())
 print(st.pop())
 print(st.getList())
+
+# Decorators example :
+
+def get_user_role() -> int:
+    return 2
+
+
+def access_control(access_level: int):
+    def outer_wrapper(func):
+        @wraps(func)
+        def inner_wrapper(*arg, **kwargs):
+            if get_user_role() <= access_level:
+                return func(*arg, **kwargs)
+            else:
+                raise PermissionError(
+                    'You do not have the proper access level.')
+        return inner_wrapper
+    return outer_wrapper
+
+
+@access_control(1)
+def deleteFile(filename):
+    print(f" {filename} is deleted")
+
+
+deleteFile("asd")
