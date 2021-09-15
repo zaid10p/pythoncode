e9325c9ff61896c4b5fd32e9cf27b2690f6c4f59
Zaid Ibrahim <zaid.ibrahim@10pearls.com>
2021-09-15 12:33:15 +0500

    regex exp

--- temp/test.py
+++ temp/test.py
@@ -1,4 +1,17 @@
+import re
 
+
+def is_filename_safe(filename):
+    # ^ means start of string and $ means end of string
+    # regex = '^[0-9]+com$'
+    regex = '^[a-zA-Z0-9][a-zA-Z0-9_()-]*(\.jpg|\.jpeg|\.png|\.gif)$'
+    # ^[a-zA-Z0-9]      start with a-zA-Z0-9
+    # [a-zA-Z0-9_()-]*      then only contains a-zA-Z0-9_()- for any number of times
+    # (\.jpg|\.jpeg|\.png|\.gif)$  at last, it must end with one of the four extensions,
+    # since we check from start to end, it can either match the whole string or none
+    return re.match(regex, filename) is not None
+
+print(is_filename_safe('as2.jpg'))
 
 def test():
     slist = ["tt", "Sdsfd", "AA", "xx", "SZ"]
@@ -8,7 +21,6 @@ def test():
     for i in g:
         print(i)
 
-
 test()
 
-print(__name__)
+#print(__name__)
